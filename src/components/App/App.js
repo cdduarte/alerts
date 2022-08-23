14eaf5d6f0cae01a376556d565dd6cc317f8bc36
cduarte <duarte.christopher@gmail.com>
2022-08-23 12:15:48 -0700

    Update indentation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,10 +9,10 @@ function App() {
     const { alertState, dispatchAddAlert, dispatchHandleAlertInput } = useAlertReducer();
 
     return (
-      <Container maxWidth="md">
-        {alertState.alerts.length > 0 && <AlertManager alerts={alertState.alerts} />}
-        <AlertsExample activeAlert={alertState.activeAlert} onSubmitAlert={dispatchAddAlert} onFormFieldChange={dispatchHandleAlertInput} />
-      </Container>
+        <Container maxWidth="md">
+          {alertState.alerts.length > 0 && <AlertManager alerts={alertState.alerts} />}
+          <AlertsExample activeAlert={alertState.activeAlert} onSubmitAlert={dispatchAddAlert} onFormFieldChange={dispatchHandleAlertInput} />
+        </Container>
     );
 }
 
